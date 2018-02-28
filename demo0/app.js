cb4b58089448ac59d1b67706bfd906730cd92bbd
guaerguagua <1025191631@qq.com>
2018-02-28 15:31:25 +0800

    app.js

--- demo0/app.js
+++ demo0/app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var path = require('path')
+var fs = require('fs')
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,5 +9,11 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
 	console.log('app is listening at port 3000');
-	console.log(process.env.NODE_ENV)
+	console.log(process.env.NODE_ENV);
+	console.log(process.cwd());
+	console.log(path.delimiter);
+	const appDirectory = fs.realpathSync(process.cwd());
+	console.log(appDirectory);
+	const folder = './package.json';
+	console.log(path.resolve(appDirectory,folder));
 });
