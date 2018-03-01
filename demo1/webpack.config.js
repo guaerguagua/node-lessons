9213d3a688e75bac1e62a8efe6b5963f3e03d617
guaerguagua <1025191631@qq.com>
2018-03-01 17:43:44 +0800

    demo1/webpack.config.js

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -1,9 +1,10 @@
 var webpack = require('webpack')
 var path =require('path')
 module.exports = {
-	entry:{
-	   app:path.join(__dirname,'src','index.js')
-	},
+	entry:[
+		'webpack-dev-server/client?http://localhost:8080/',
+	   	path.join(__dirname,'src','index.js')	
+	],
 	plugins:[
         new webpack.HotModuleReplacementPlugin()
     ],
